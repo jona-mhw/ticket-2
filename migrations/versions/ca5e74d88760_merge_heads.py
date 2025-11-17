548d9b24df1ce88ba02acf13effbfc98be63860c
Jonathan Segura Sistemas <jonathansistemas@avansalud.cl>
2025-11-17 14:05:33 -0300

    Add migration: Merge heads

---
+++ migrations/versions/ca5e74d88760_merge_heads.py
@@ -0,0 +1,24 @@
+"""Merge heads
+
+Revision ID: ca5e74d88760
+Revises: 202511151930, d4f7b118cd7e
+Create Date: 2025-11-17 13:44:27.245681
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ca5e74d88760'
+down_revision = ('202511151930', 'd4f7b118cd7e')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
